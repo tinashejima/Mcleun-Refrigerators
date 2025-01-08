6a7557dd4f9ed2de8e30bbd4e1a653a6317dc059
tinashejima <jimatinashe@gmail.com>
2025-01-08 22:05:15 +0200

    add files

---
+++ mcleunApp/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Header from './components/Header.jsx';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <AboutUs />
+      <Services />
+      <TargetMarket />
+      <Footer />
+      </>
+    
+  );
+  
+}
+
+export default App
