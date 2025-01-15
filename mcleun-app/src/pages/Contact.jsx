7df013c73bd0afc450d93824b7aaaaf623f5f8e7
tinashejima <jimatinashe@gmail.com>
2025-01-15 13:27:12 +0200

    add files

--- mcleun-app/src/pages/Contact.jsx
+++ mcleun-app/src/pages/Contact.jsx
@@ -1,11 +1,52 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import { FaPhone, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
+
+
 function Contact() {
 
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate('/');
+    };
+
     return (
+        <div className="contact-container">
+            <h1 className="contact-title">Contact Information</h1>
 
-        <div>
+            <div className="contact-content">
+                {/* Emergency Call-to-Action */}
+                <div className="emergency-contact">
+                    <p className="emergency-text">
+                        Call us now for <span className="highlight">Emergency Service</span>:
+                    </p>
+                    <p className="phone-numbers">
+                        <FaPhone className="icon" /> +263 789 654 527 / +263 787 799 867
+                    </p>
+                </div>
 
-            <h1>Contact Info</h1>
+
+                <div className="contact-details">
+                    <div className="contact-item">
+                        <FaPhone className="icon" />
+                        <p>Phone: +263 789 654 527</p>
+                    </div>
+                    <div className="contact-item">
+                        <FaEnvelope className="icon" />
+                        <p>Email: mcleunrefrigeration23@gmail.com</p>
+                    </div>
+                    <div className="contact-item">
+                        <FaMapMarkerAlt className="icon" />
+                        <p>Address: P.O.Box 94 Hwange, Zimbabwe</p>
+                    </div>
+                </div>
+                <button className="back-button" onClick={handleGoBack}>
+                    Back to Home
+                </button>
+            </div>
         </div>
     );
 }
+
 export default Contact;
