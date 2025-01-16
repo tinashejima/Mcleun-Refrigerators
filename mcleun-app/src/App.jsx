bff5f5d9a5215790846ee9677360cbc68685c04c
tinashejima <jimatinashe@gmail.com>
2025-01-16 11:28:07 +0200

    add code changes

--- mcleun-app/src/App.jsx
+++ mcleun-app/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
 
     <>
-      <Router basename="/Mcleun-Refrigerators">
+      <Router>
 
         {/* <Navbar /> */}
         <Routes>
--- mcleun-app/vite.config.js
+++ mcleun-app/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mcleun-Refrigerators/mcleun-app/dist/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
